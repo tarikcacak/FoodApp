28fd5c0c0346395240ed8f5d16d8bb5e4e20b278
Tarik Čačković <113112797+tarikcacak@users.noreply.github.com>
2023-10-23 21:16:12 +0200

    Started building food app

---
+++ app/src/main/java/com/example/foodapp/ui/activites/LoginRegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.foodapp.ui.activites
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
