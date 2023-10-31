bc08bbbd531a21e8cf985e85d5e7288f2700f698
Tarik Čačković <113112797+tarikcacak@users.noreply.github.com>
2023-10-31 18:50:05 +0100

    finished search fragment UI

--- app/src/main/java/com/example/foodapp/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/example/foodapp/fragments/search/SearchFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.foodapp.R
+import com.example.airmovies.util.Resource
 import com.example.foodapp.databinding.FragmentSearchBinding
 import com.example.foodapp.fragments.search.adapter.SearchAdapter
+import com.example.foodapp.models.meal.SearchResult
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
 
@@ -29,10 +36,23 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setRecyclerView()
+        getSearchedData()
     }
 
-    private fun setRecyclerView() {
+    private fun getSearchedData() {
+        var searchJob: Job? = null
+        binding.etSearchBar.addTextChangedListener { query ->
+            searchJob?.cancel()
+            searchJob = lifecycleScope.launch {
+                delay(500)
+                viewModel.getSearchedMeals(query.toString())
+                prepareRecyclerView()
+                observeSearchedMeals()
+            }
+        }
+    }
+
+    private fun prepareRecyclerView() {
         searchAdapter = SearchAdapter()
         binding.recViewSearch.apply {
             layoutManager = GridLayoutManager(context, 1, GridLayoutManager.VERTICAL, false)
@@ -41,7 +61,22 @@ class SearchFragment : Fragment() {
     }
 
     private fun observeSearchedMeals() {
+        viewModel.searchedMealsList.observe(viewLifecycleOwner) {
+            when (it) {
+                is Resource.Loading -> {
 
+                }
+                is Resource.Error -> {
+                    Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT).show()
+                }
+                is Resource.Success -> {
+                    if (it.data?.results != null) {
+                        searchAdapter.setMeals(it.data.results as ArrayList<SearchResult>)
+                    }
+                }
+                else -> Unit
+            }
+        }
     }
 
 }
